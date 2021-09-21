c55c3b4a5a99551a3125ed0104f1d542971edbe0
Kevin Gleijm <0981227@hr.nl>
2021-09-21 11:00:05 +0200

    -Cleanup code -Add quality of life functions -Simulate terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,32 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
-#include <Arduino_APDS9960.h>
-#include <Arduino_HTS221.h>
 
+// string of 128 chars to force bluetooth buffer length
+static constexpr char* emptyBuffer = "|NO_MSG|                                                                                                                        ";
 
+// Name that is displayed when scanning for bluetooth devices
+static constexpr char* nameOfPeripheral = "ComunicationPeripheral";
+
+// Hardcoded BLE uuis's
 static constexpr char* uuidOfService = "0000181a-0000-1000-8000-00805f9b34fb";
 static constexpr char* uuidOfRxChar = "00002A3D-0000-1000-8000-00805f9b34fb";
 static constexpr char* uuidOfTxChar = "00002A58-0000-1000-8000-00805f9b34fb";
-static constexpr char* nameOfPeripheral = "ComunicationPeripheral";
 
-static const int RX_BUFFER_SIZE = 256;
-static const bool RX_BUFFER_FIXED_LENGTH = false;
+// Constants handling buffersizes
+static const int BUFFER_SIZE = 128;
+static const bool BUFFER_FIXED_LENGTH = false;
 
-static BLECharacteristic rxChar(uuidOfRxChar, BLEWriteWithoutResponse | BLEWrite, RX_BUFFER_SIZE, RX_BUFFER_FIXED_LENGTH);
-static BLECharacteristic txChar(uuidOfTxChar, BLERead | BLENotify | BLEBroadcast, RX_BUFFER_SIZE, RX_BUFFER_FIXED_LENGTH);
+// Initializing characteristics used for recieving and transmitting messages
+static BLECharacteristic rxChar(uuidOfRxChar, BLEWriteWithoutResponse | BLEWrite, BUFFER_SIZE, BUFFER_FIXED_LENGTH);
+static BLECharacteristic txChar(uuidOfTxChar, BLERead | BLENotify | BLEBroadcast, BUFFER_SIZE, BUFFER_FIXED_LENGTH);
 static BLEService communicationService(uuidOfService);
 
+// Static Wrapper class for BLE functions
 class BLECommunicationManager{
 
   private:
     
+    //wrapper for BLE startup sequence
     static void startBLE() {
       if (!BLE.begin())
       {
@@ -28,37 +35,67 @@ class BLECommunicationManager{
       }
     }
 
+    // Triggers when connection to the central device is established
     static void onBLEConnected(BLEDevice central) {
       Serial.print("Connected event, central: ");
       Serial.println(central.address());
     }
 
+    // Triggers when connection to the central device is lost
     static void onBLEDisconnected(BLEDevice central) {
       Serial.print("Disconnected event, central: ");
       Serial.println(central.address());
     }
 
+    // Triggers before central device reads the characteristic
     static void onTxRead(BLEDevice central, BLECharacteristic characteristic){
       Serial.println("Detected TX Read");
     }
 
+    // Triggers when central device writes to this peripheral
     static void onRxWrite(BLEDevice central, BLECharacteristic characteristic){
-        // central wrote new value to characteristic, update LED
-        Serial.print("Characteristic event, read: ");
-        byte tmp[256];
-        int dataLength = rxChar.readValue(tmp, 256);
-
+        Serial.print("Received message from central device");
+        byte tmp[BUFFER_SIZE];
+        int dataLength = rxChar.readValue(tmp, BUFFER_SIZE);
+        String incoming = "";
         for(int i = 0; i < dataLength; i++) {
-          Serial.print((char)tmp[i]);
+          incoming += (char)tmp[i];
         }
-        Serial.println();
-        Serial.print("Value length = ");
-        Serial.println(rxChar.valueLength());
+        BLECommunicationManager::interpretMessage(incoming);
     }
 
+    // Interpret message coming from central device
+    static void interpretMessage(String incomingMessage){
+      // do something with custom commands from the central device
+      if (incomingMessage.substring(0,4) == "Foo"){
+        Serial.println("\nMessage received: Foo!\n");
+        setOutgoingTo("Readed Foo loud and clear!");
+      }else if (incomingMessage.substring(0,4) == "Bar"){
+        Serial.println("\nMessage received: Bar!\n");
+        setOutgoingTo("Going for bar this time!");
+      }else{
+        Serial.println("Unknown command received: ");
+        Serial.println(incomingMessage);
+      }
+    }
 
   public:
+
+    // Set outgoing message to string x
+    static void setOutgoingTo(String outgoing){
+      char outgoingArr[BUFFER_SIZE];
+      outgoing.toCharArray(outgoingArr, BUFFER_SIZE);
+      setOutgoingTo(outgoingArr);
+    }
+
+    // Set outgoing to char array x
+    static void setOutgoingTo(char outgoing[]){
+      txChar.setValue(outgoing);
+      // Serial.println("Set tx to:");
+      // Serial.println(outgoing);
+    }
     
+    // Set up BLE communication variables
     static void begin(){
       
     
@@ -94,21 +131,18 @@ class BLECommunicationManager{
       Serial.println(uuidOfTxChar);
       Serial.println("Bluetooth device active, waiting for connections...");
 
-
-      rxChar.setValue("I'm Rx");
-      txChar.setValue("I'm TX");
-
+      rxChar.setValue(emptyBuffer);
+      txChar.setValue(emptyBuffer);
     }
 
+    // Do additional stuff while device is connected
     static void onLoop(){
-      // put your main code here, to run repeatedly:
       BLEDevice central = BLE.central();
       
       if (central)
       {
-        // Only send data if we are connected to a central device.
         while (central.connected()) {
-          // Serial.println("Device is connected in mainloop");
+          // Do stuff here when central device is connected.
         }
       }
     }
@@ -117,8 +151,12 @@ class BLECommunicationManager{
 
 void setup() {
 
+  // setup with delays to prevent serial flooding
+  delay(1000);
   Serial.begin(9600);
   while (!Serial);
+  Serial.println("Serial started");
+  delay(1000);
 
   BLECommunicationManager::begin();
 
@@ -127,7 +165,10 @@ void setup() {
 void loop() {
   BLECommunicationManager::onLoop();
   
-  
+  // you can do whatever in your loop
+
+  delay(1000);
+
 }
 
 
